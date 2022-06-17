615d24f077eb60687507b4f463ecced5caee6d75
UrosVesic <uros99uki@gmail.com>
2022-06-17 20:36:34 +0200

    Dodat custom runtime exception

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/exception/MyRuntimeException.java
@@ -0,0 +1,7 @@
+package rs.ac.bg.fon.springsocialnetwork.exception;
+
+public class MyRuntimeException extends  RuntimeException{
+    public MyRuntimeException(String message) {
+        super(message);
+    }
+}
