0ca338a0e8170f9939587b6aceb06ecdd93ddffe
UrosVesic <uros99uki@gmail.com>
2022-06-17 22:17:45 +0200

    Dodata metoda findByUsername

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/UserRepository.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/UserRepository.java
@@ -3,5 +3,8 @@ package rs.ac.bg.fon.springsocialnetwork.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rs.ac.bg.fon.springsocialnetwork.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
 }
