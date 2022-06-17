9925fe0b7889794ffe88bc38fc2fe73d42a93d8c
UrosVesic <uros99uki@gmail.com>
2022-06-17 22:18:42 +0200

    Dodata klasa JwtProvider koja generise jwt

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/jwt/JwtProvider.java
@@ -0,0 +1,45 @@
+package rs.ac.bg.fon.springsocialnetwork.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import rs.ac.bg.fon.springsocialnetwork.exception.MyRuntimeException;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+import org.springframework.security.core.userdetails.User;
+
+@Service
+public class JwtProvider {
+
+    private KeyStore keyStore;
+
+    @PostConstruct
+    public void init() throws KeyStoreException {
+        keyStore = KeyStore.getInstance("JKS");
+        InputStream resourceAsStream = getClass().getResourceAsStream("/springsm.jks");
+        try {
+            keyStore.load(resourceAsStream,"uros99".toCharArray());
+        } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+            throw new MyRuntimeException("Exception occured while loading keystore");
+        }
+    }
+
+    public String generateToken(Authentication authentication){
+        User principal = (User) authentication.getPrincipal();
+        return Jwts.builder().setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+
+    private Key getPrivateKey() {
+        try {
+            return keyStore.getKey("springsm","uros99".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new MyRuntimeException("Exception occured while reading private key from keystore");
+        }
+    }
+}
