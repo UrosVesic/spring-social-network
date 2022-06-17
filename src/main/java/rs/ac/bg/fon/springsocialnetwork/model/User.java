4838b0567f8e9c153a54bb0f7d5d348cd13efacd
UrosVesic <uros99uki@gmail.com>
2022-06-17 20:26:37 +0200

    Dodata entity klasa User i Spring boot starter validation

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/model/User.java
@@ -0,0 +1,32 @@
+package rs.ac.bg.fon.springsocialnetwork.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    @NotBlank(message = "Email is required")
+    private String email;
+    private Instant created;
+
+}
