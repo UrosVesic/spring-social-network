5bac57e8778d49435b295343902cce3e3e031d98
UrosVesic <uros99uki@gmail.com>
2022-06-18 00:38:11 +0200

    Dodata lista pratilaca i osoba koje prati kao atributi

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/model/User.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/model/User.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -28,5 +26,13 @@ public class User {
     @NotBlank(message = "Email is required")
     private String email;
     private Instant created;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "following",
+            joinColumns = @JoinColumn(name = "following_user_id"),
+            inverseJoinColumns = @JoinColumn(name = "followed_user_id"))
+    private List<User> following;
+
+    @ManyToMany(mappedBy = "following",fetch = FetchType.LAZY)
+    private List<User> followers;
 
 }
