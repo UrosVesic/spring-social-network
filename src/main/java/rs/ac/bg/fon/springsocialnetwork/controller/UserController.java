51508524ee622fc7456a728191df4f798a37e39c
UrosVesic <uros99uki@gmail.com>
2022-06-18 01:59:37 +0200

    Dodata api ruta za unfollow i exception handler za MyRuntimeException

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/UserController.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.bg.fon.springsocialnetwork.dto.UserDto;
+import rs.ac.bg.fon.springsocialnetwork.exception.MyRuntimeException;
 import rs.ac.bg.fon.springsocialnetwork.model.User;
 import rs.ac.bg.fon.springsocialnetwork.service.UserService;
 
@@ -20,10 +21,21 @@ public class UserController {
         userService.follow(idFollowing,idFollowed);
         return new ResponseEntity<>("Follow succesful", HttpStatus.CREATED);
     }
+    @DeleteMapping("/{idFollowing}/unfollow/{idFollowed}")
+    public ResponseEntity<String> unfollow(@PathVariable Long idFollowing,@PathVariable Long idFollowed){
+        userService.unfollow(idFollowing,idFollowed);
+        return new ResponseEntity<>("Unfollow succesful", HttpStatus.OK);
+    }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable Long id){
         UserDto userDto = userService.getUser(id);
         return new ResponseEntity<>(userDto,HttpStatus.OK);
+    }
+
+    @ExceptionHandler(MyRuntimeException.class)
+    public  ResponseEntity<String> handleMyRuntimeException(MyRuntimeException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
     }
 }
