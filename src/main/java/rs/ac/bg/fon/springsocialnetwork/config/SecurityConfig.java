65b9ddc9ed0b81351a48b5dc501c720eb6a07542
UrosVesic <uros99uki@gmail.com>
2022-06-17 22:19:26 +0200

    Dodat auth manager bean

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/config/SecurityConfig.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package rs.ac.bg.fon.springsocialnetwork.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import rs.ac.bg.fon.springsocialnetwork.service.UserDetailsServiceImpl;
 
 @EnableWebSecurity
+@AllArgsConstructor
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsService userDetailsService;
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,9 +35,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
