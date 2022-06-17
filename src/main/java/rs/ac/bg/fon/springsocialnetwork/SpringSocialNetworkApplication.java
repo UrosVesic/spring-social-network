35e944d93b19f90aea81c3a793467702bdbf3dae
UrosVesic <uros99uki@gmail.com>
2022-06-17 20:20:46 +0200

    Initial commit

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/SpringSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.springsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/springsocialnetwork/SpringSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.springsocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
