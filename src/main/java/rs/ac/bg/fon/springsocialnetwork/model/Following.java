8c42841ef86585f48705ec43d5f561dcedb83623
UrosVesic <uros99uki@gmail.com>
2022-06-18 00:39:55 +0200

    Entity klasa koja predstavlja jedno pracenje

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/model/Following.java
@@ -0,0 +1,29 @@
+package rs.ac.bg.fon.springsocialnetwork.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import rs.ac.bg.fon.springsocialnetwork.model.idclasses.FollowingId;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@IdClass(FollowingId.class)
+public class Following {
+    @Id
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "following_user_id", nullable = false)
+    private User following;
+    @Id
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "followed_user_id", nullable = false)
+    private User followed;
+    private Instant followedAt;
+
+
+
+}
