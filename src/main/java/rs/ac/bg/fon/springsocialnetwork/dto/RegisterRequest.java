99a2aefbf1b0e751e2c7eb32f7d0b802cc14872c
UrosVesic <uros99uki@gmail.com>
2022-06-17 20:36:51 +0200

    Dodata dto klasa za registraciju

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package rs.ac.bg.fon.springsocialnetwork.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String username;
+    private String password;
+}
