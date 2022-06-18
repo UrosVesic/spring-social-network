de9f29890005b30720133c8214d7fbfe1b691840
UrosVesic <uros99uki@gmail.com>
2022-06-18 02:00:39 +0200

    Dto klasa za entity klasu user

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/dto/UserDto.java
@@ -0,0 +1,20 @@
+package rs.ac.bg.fon.springsocialnetwork.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long userId;
+    private String username;
+    private String email;
+    private Instant created;
+    private int numOfFollowers;
+    private int numOfFollowing;
+}
