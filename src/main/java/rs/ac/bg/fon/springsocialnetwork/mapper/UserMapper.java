1152be9f64cb6559d3642ad1dc93595456de37d1
UrosVesic <uros99uki@gmail.com>
2022-06-18 20:26:32 +0200

    Klasa koja mapira dto u entity i obrnuto

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package rs.ac.bg.fon.springsocialnetwork.mapper;
+
+import rs.ac.bg.fon.springsocialnetwork.dto.UserDto;
+import rs.ac.bg.fon.springsocialnetwork.model.User;
+
+public class UserMapper implements GenericMapper<UserDto, User>{
+
+    @Override
+    public User toEntity(UserDto dto) {
+        User user = new User();
+        user.setUserId(dto.getUserId());
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+        user.setCreated(dto.getCreated());
+        return user;
+    }
+
+    @Override
+    public UserDto toDto(User user) {
+        return new UserDto(user.getUserId(),user.getUsername(),user.getEmail(),user.getCreated(),user.getFollowing().size(),user.getFollowers().size());
+    }
+}
