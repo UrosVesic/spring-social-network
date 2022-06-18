f53ccabc5efc44693b876070b7d876ea02f496ed
UrosVesic <uros99uki@gmail.com>
2022-06-18 02:02:43 +0200

    Implementirana api ruta za vracanje svih pratilaca jednog korisnika

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/FollowingController.java
@@ -0,0 +1,27 @@
+package rs.ac.bg.fon.springsocialnetwork.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.ac.bg.fon.springsocialnetwork.dto.UserDto;
+import rs.ac.bg.fon.springsocialnetwork.model.User;
+import rs.ac.bg.fon.springsocialnetwork.service.FollowingService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/following")
+@AllArgsConstructor
+public class FollowingController {
+
+    private FollowingService followingService;
+
+    @GetMapping("/followers/{userId}")
+    public List<UserDto> getFollowersForUser(@PathVariable Long userId){
+        return followingService.getFollowersForUser(userId);
+
+    }
+}
---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/service/FollowingService.java
@@ -0,0 +1,33 @@
+package rs.ac.bg.fon.springsocialnetwork.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import rs.ac.bg.fon.springsocialnetwork.dto.UserDto;
+import rs.ac.bg.fon.springsocialnetwork.model.Following;
+import rs.ac.bg.fon.springsocialnetwork.model.User;
+import rs.ac.bg.fon.springsocialnetwork.repository.FollowRepository;
+import rs.ac.bg.fon.springsocialnetwork.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class FollowingService {
+
+    private FollowRepository followRepository;
+
+
+    public List<UserDto> getFollowersForUser(Long userId) {
+        List<Following> optFoll = followRepository.findAllByFollowed_userId(userId);
+        List<User> followers = optFoll.stream().map(Following::getFollowing).collect(Collectors.toList());
+        List<UserDto> followersDto = new ArrayList<>();
+        for (User u:followers){
+            UserDto userDto = new UserDto(u.getUserId(),u.getUsername(),u.getEmail(),u.getCreated(),u.getFollowing().size(),u.getFollowers().size());
+            followersDto.add(userDto);
+        }
+        return followersDto;
+    }
+}
