fb4a0d991bda5e2aee4968781ce12198afc97dac
UrosVesic <uros99uki@gmail.com>
2022-06-18 02:02:05 +0200

    Id klasa za kompozitni primarni kljuc Following objekta

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/model/idclasses/FollowingId.java
@@ -0,0 +1,20 @@
+package rs.ac.bg.fon.springsocialnetwork.model.idclasses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import rs.ac.bg.fon.springsocialnetwork.model.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.IdClass;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FollowingId implements Serializable {
+
+    private Long following;
+    private Long followed;
+
+}
