66867d424c3c8fb5fe88e42afb5ea9ffb8a748d8
UrosVesic <uros99uki@gmail.com>
2022-06-18 02:01:43 +0200

    Dodat interface FollowRepository za manipulaciju Following entitetima

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/FollowRepository.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.springsocialnetwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.ac.bg.fon.springsocialnetwork.model.Following;
+import rs.ac.bg.fon.springsocialnetwork.model.idclasses.FollowingId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Following, FollowingId> {
+
+    List<Following> findAllByFollowed_userId(Long id);
+}
