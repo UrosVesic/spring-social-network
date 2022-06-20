825a9789f5ad82509bfdf3a2853151057f6cb3b2
UrosVesic <uros99uki@gmail.com>
2022-06-20 18:51:10 +0200

    Konfiguracija cors policy-a

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/config/WebConfig.java
@@ -0,0 +1,27 @@
+package rs.ac.bg.fon.springsocialnetwork.config;/**
+ * @author UrosVesic
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author UrosVesic
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
