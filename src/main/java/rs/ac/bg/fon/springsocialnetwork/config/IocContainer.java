1c599e1ae17b84b58579c7487ed6059767ff54e4
UrosVesic <uros99uki@gmail.com>
2022-06-21 13:22:27 +0200

    Dodati bean-ovi za PostMapper i TopicMapper

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/config/IocContainer.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/config/IocContainer.java
@@ -2,12 +2,28 @@ package rs.ac.bg.fon.springsocialnetwork.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import rs.ac.bg.fon.springsocialnetwork.mapper.PostMapper;
+import rs.ac.bg.fon.springsocialnetwork.mapper.TopicMapper;
 import rs.ac.bg.fon.springsocialnetwork.mapper.UserMapper;
+import rs.ac.bg.fon.springsocialnetwork.model.Topic;
+import rs.ac.bg.fon.springsocialnetwork.repository.CommentRepository;
+import rs.ac.bg.fon.springsocialnetwork.repository.PostRepository;
+import rs.ac.bg.fon.springsocialnetwork.repository.ReactionRepository;
 
 @Configuration
 public class IocContainer {
     @Bean
     public UserMapper userMapper(){
         return new UserMapper();
+    }
+
+    @Bean
+    public PostMapper postMapper(CommentRepository commentRepository, ReactionRepository reactionRepository){
+        return new PostMapper(commentRepository,reactionRepository);
+    }
+
+    @Bean
+    public TopicMapper topicMapper(PostRepository postRepository){
+        return new TopicMapper(postRepository);
     }
 }
