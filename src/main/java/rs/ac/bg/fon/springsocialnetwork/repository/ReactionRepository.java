f57d463cae1c8e6e76f8171989925d0ee5476b82
UrosVesic <uros99uki@gmail.com>
2022-06-21 13:26:38 +0200

    Repozitorijum interfejs za rad sa reakcijama(lajkovi,dislajkovi)

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/ReactionRepository.java
@@ -0,0 +1,12 @@
+package rs.ac.bg.fon.springsocialnetwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.ac.bg.fon.springsocialnetwork.model.Post;
+import rs.ac.bg.fon.springsocialnetwork.model.Reaction;
+import rs.ac.bg.fon.springsocialnetwork.model.ReactionType;
+
+import java.util.List;
+
+public interface ReactionRepository extends JpaRepository<Reaction,Long> {
+    List<Reaction> findByPostAndReactionType(Post post, ReactionType like);
+}
