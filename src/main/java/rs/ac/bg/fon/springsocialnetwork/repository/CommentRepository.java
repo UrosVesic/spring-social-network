35a0c5fb9274fdc9fc3997f62fc9967070b835b6
UrosVesic <uros99uki@gmail.com>
2022-06-21 13:22:05 +0200

    Dodat repozitorijum za rad sa komentarima

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package rs.ac.bg.fon.springsocialnetwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.ac.bg.fon.springsocialnetwork.model.Comment;
+import rs.ac.bg.fon.springsocialnetwork.model.Post;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment> findAllByPost(Post post);
+}
