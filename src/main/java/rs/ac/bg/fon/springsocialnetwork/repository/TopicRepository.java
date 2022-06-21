f66dc0895a4102ffa1ba759822e21ed670a3e02c
UrosVesic <uros99uki@gmail.com>
2022-06-21 13:28:11 +0200

    Repozitorijum interfejs za rad sa temama

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/TopicRepository.java
@@ -0,0 +1,7 @@
+package rs.ac.bg.fon.springsocialnetwork.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.ac.bg.fon.springsocialnetwork.model.Topic;
+
+public interface TopicRepository extends JpaRepository<Topic,Long> {
+}
