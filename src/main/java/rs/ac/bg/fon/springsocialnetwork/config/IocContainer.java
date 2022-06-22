484cb5baf5c8aee94543976f3065d18dc67dec4c
UrosVesic <uros99uki@gmail.com>
2022-06-22 03:07:15 +0200

    Dodat PostRequestMapper

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/config/IocContainer.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/config/IocContainer.java
@@ -2,13 +2,16 @@ package rs.ac.bg.fon.springsocialnetwork.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import rs.ac.bg.fon.springsocialnetwork.mapper.PostMapper;
+import rs.ac.bg.fon.springsocialnetwork.mapper.PostRequestMapper;
+import rs.ac.bg.fon.springsocialnetwork.mapper.PostResponseMapper;
 import rs.ac.bg.fon.springsocialnetwork.mapper.TopicMapper;
 import rs.ac.bg.fon.springsocialnetwork.mapper.UserMapper;
-import rs.ac.bg.fon.springsocialnetwork.model.Topic;
 import rs.ac.bg.fon.springsocialnetwork.repository.CommentRepository;
 import rs.ac.bg.fon.springsocialnetwork.repository.PostRepository;
 import rs.ac.bg.fon.springsocialnetwork.repository.ReactionRepository;
+import rs.ac.bg.fon.springsocialnetwork.repository.TopicRepository;
+import rs.ac.bg.fon.springsocialnetwork.service.AuthService;
+
 /**
  * @author UrosVesic
  */
@@ -20,12 +23,17 @@ public class IocContainer {
     }
 
     @Bean
-    public PostMapper postMapper(CommentRepository commentRepository, ReactionRepository reactionRepository){
-        return new PostMapper(commentRepository,reactionRepository);
+    public PostResponseMapper postMapper(CommentRepository commentRepository, ReactionRepository reactionRepository){
+        return new PostResponseMapper(commentRepository,reactionRepository);
     }
 
     @Bean
-    public TopicMapper topicMapper(PostRepository postRepository){
-        return new TopicMapper(postRepository);
+    public TopicMapper topicMapper(PostRepository postRepository, AuthService authService){
+        return new TopicMapper(postRepository,authService);
+    }
+
+    @Bean
+    public PostRequestMapper PostRequestMapper(TopicRepository topicRepository){
+        return new PostRequestMapper(topicRepository);
     }
 }
