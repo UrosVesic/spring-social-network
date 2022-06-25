51a36cfdb3fc1371b7e5d7489ca48b0cc06e1237
UrosVesic <uros99uki@gmail.com>
2022-06-25 14:03:22 +0200

    Dodata metoda findByPost_idAndUser(Long,User)

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/ReactionRepository.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/ReactionRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import rs.ac.bg.fon.springsocialnetwork.model.Post;
 import rs.ac.bg.fon.springsocialnetwork.model.Reaction;
 import rs.ac.bg.fon.springsocialnetwork.model.ReactionType;
+import rs.ac.bg.fon.springsocialnetwork.model.User;
+
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author UrosVesic
@@ -12,4 +15,6 @@ import java.util.List;
 
 public interface ReactionRepository extends JpaRepository<Reaction,Long> {
     List<Reaction> findByPostAndReactionType(Post post, ReactionType like);
+
+    Optional<Reaction> findByPost_idAndUser(Long postId, User currentUser);
 }
