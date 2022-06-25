0a62f2fcfdfb525b034d274e246ecf5e7abb70de
UrosVesic <uros99uki@gmail.com>
2022-06-25 14:03:04 +0200

    Dodata metoda findByPost_id(Long)

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/CommentRepository.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/CommentRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findAllByPost(Post post);
+
+    List<Comment> findByPost_id(Long postId);
 }
