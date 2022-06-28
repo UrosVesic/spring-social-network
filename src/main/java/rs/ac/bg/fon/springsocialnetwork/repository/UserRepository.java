8c911e388e398244f9d25cf83c3b9ebca0591795
UrosVesic <uros99uki@gmail.com>
2022-06-28 14:48:07 +0200

    Dodata metoda findByuserIdNotIn

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/UserRepository.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package rs.ac.bg.fon.springsocialnetwork.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rs.ac.bg.fon.springsocialnetwork.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -10,4 +13,7 @@ import java.util.Optional;
  */
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
+
+    List<User> findByuserIdNotIn(List<Long> following);
+
 }
