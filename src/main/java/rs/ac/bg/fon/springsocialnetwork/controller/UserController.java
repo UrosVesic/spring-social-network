935f3ac173d6c06d1b1e7737248bd10accb23cb8
UrosVesic <uros99uki@gmail.com>
2022-06-28 14:46:03 +0200

    Dodate nove api rute

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/UserController.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.bg.fon.springsocialnetwork.dto.UserDto;
 import rs.ac.bg.fon.springsocialnetwork.exception.MyRuntimeException;
-import rs.ac.bg.fon.springsocialnetwork.model.User;
 import rs.ac.bg.fon.springsocialnetwork.service.AuthService;
 import rs.ac.bg.fon.springsocialnetwork.service.UserService;
 
@@ -23,15 +22,21 @@ public class UserController {
     private UserService userService;
     private AuthService authService;
 
-    @PostMapping("/follow/{idFollowed}")
-    public ResponseEntity<String> follow(@PathVariable Long idFollowed){
-        userService.follow(authService.getCurrentUser(), idFollowed);
-        return new ResponseEntity<>("Follow succesful", HttpStatus.CREATED);
+    @PostMapping(value = "/follow/{username}")
+    public ResponseEntity<String> follow(@PathVariable String username){
+        userService.follow(authService.getCurrentUser(), username);
+        return new ResponseEntity<>( HttpStatus.CREATED);
+    }
+
+    @PostMapping("/unfollow/{username}")
+    public ResponseEntity unfollow(@PathVariable String username){
+        userService.unfollow(authService.getCurrentUser(), username);
+        return new ResponseEntity<>( HttpStatus.CREATED);
     }
     @DeleteMapping("/{idFollowing}/unfollow/{idFollowed}")
-    public ResponseEntity<String> unfollow(@PathVariable Long idFollowing,@PathVariable Long idFollowed){
+    public ResponseEntity unfollow(@PathVariable Long idFollowing,@PathVariable Long idFollowed){
         userService.unfollow(idFollowing,idFollowed);
-        return new ResponseEntity<>("Unfollow succesful", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
@@ -41,9 +46,30 @@ public class UserController {
         return new ResponseEntity<>(userDto,HttpStatus.OK);
     }
 
-    @GetMapping("/followers/{userId}")
+   /* @GetMapping("/followers/{userId}")
     public ResponseEntity<List<UserDto>> getAllFollowersForUser(@PathVariable Long userId){
         return new ResponseEntity<>(userService.getAllFollowersForUser(userId),HttpStatus.OK);
+    }*/
+
+    @GetMapping("/profile-info/{username}")
+    public ResponseEntity<UserDto> getProfileInfo(@PathVariable String username){
+        UserDto userResponse=userService.getProfileInfo(username);
+        return new ResponseEntity<UserDto>(userResponse,HttpStatus.OK);
+    }
+    @GetMapping("/suggested")
+    public ResponseEntity<List<UserDto>> getAllSuggestedUsers(){
+        List<UserDto> users = userService.getAllSuggestedUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+
+    @GetMapping("/followers/{username}")
+    public ResponseEntity<List<UserDto>> getAllFollowersForUser(@PathVariable String username){
+        return new ResponseEntity<>(userService.getAllFollowersForUser(username),HttpStatus.OK);
+    }
+
+    @GetMapping("/following/{username}")
+    public ResponseEntity<List<UserDto>> getAllFollowingForUser(@PathVariable String username){
+        return new ResponseEntity<>(userService.getAllFollowingForUser(username),HttpStatus.OK);
     }
 
     @ExceptionHandler(MyRuntimeException.class)
