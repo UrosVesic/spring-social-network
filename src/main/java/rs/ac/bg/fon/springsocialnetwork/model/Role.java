6ff8c5f3e948a20ec9e1845c41c58e86ea9cb10c
UrosVesic <uros99uki@gmail.com>
2022-07-04 00:27:07 +0200

    Dodata entity klasa Role koja predstavlja ulogu korisnika u sistemu

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/model/Role.java
@@ -0,0 +1,48 @@
+package rs.ac.bg.fon.springsocialnetwork.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author UrosVesic
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+    private String name;
+    private String details;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
