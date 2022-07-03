2218946ea3fc9e6313a0255731de150e7fb45a7b
UrosVesic <uros99uki@gmail.com>
2022-07-04 00:24:05 +0200

    Dodat PostReport entity klasa

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/model/PostReport.java
@@ -0,0 +1,27 @@
+package rs.ac.bg.fon.springsocialnetwork.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author UrosVesic
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostReport implements MyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId",referencedColumnName = "id")
+    private Post post;
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId",referencedColumnName = "userId")
+    private User user;
+    private ReportStatus reportStatus;
+}
