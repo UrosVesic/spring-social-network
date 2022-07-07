27b05585117cc1822f128dc488e95550d1069d24
UrosVesic <uros99uki@gmail.com>
2022-07-08 01:56:09 +0200

    Changes

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/SpringSocialNetworkApplication.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/SpringSocialNetworkApplication.java
@@ -2,6 +2,7 @@ package rs.ac.bg.fon.springsocialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SpringSocialNetworkApplication {
