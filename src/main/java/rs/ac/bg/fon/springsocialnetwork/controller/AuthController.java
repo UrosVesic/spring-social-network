e6207c1dad6f2c0e3e48ca611cc8c47188606d38
UrosVesic <uros99uki@gmail.com>
2022-07-10 00:36:06 +0200

    Dodati exception handleri

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/AuthController.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/AuthController.java
@@ -3,13 +3,23 @@ package rs.ac.bg.fon.springsocialnetwork.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.bg.fon.springsocialnetwork.dto.AuthResponse;
 import rs.ac.bg.fon.springsocialnetwork.dto.LoginRequest;
 import rs.ac.bg.fon.springsocialnetwork.dto.RegisterRequest;
 import rs.ac.bg.fon.springsocialnetwork.service.AuthService;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author UrosVesic
@@ -21,21 +31,42 @@ public class AuthController {
 
     private AuthService authService;
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<String> signup(@RequestBody @Valid RegisterRequest registerRequest){
         authService.signup(registerRequest);
         return new ResponseEntity<>("Registration succesfull", HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest loginRequest){
+    public AuthResponse login(@RequestBody @Valid LoginRequest loginRequest){
         return authService.login(loginRequest);
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public  ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException ex){
+        Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
+        List<String> violations=new ArrayList<>();
+        violations.addAll(constraintViolations.stream().map(viol->viol.getMessage()).collect(Collectors.toList()));
+        return new ResponseEntity<>(violations,HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public  ResponseEntity<String> handleSQLIntegrityConstraintViolation(SQLIntegrityConstraintViolationException ex){
         return new ResponseEntity<>("Username or email already exists",HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public  ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<ObjectError> allErrors = ex.getAllErrors();
+        List<String> collect = allErrors.stream().map(err -> err.getDefaultMessage()).collect(Collectors.toList());
+        return new ResponseEntity<>(collect,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> authExceptionHandler(AuthenticationException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.FORBIDDEN);
+    }
+
+
 
 
 }
--- src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/PostController.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/controller/PostController.java
@@ -3,16 +3,22 @@ package rs.ac.bg.fon.springsocialnetwork.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.bg.fon.springsocialnetwork.dto.PostRequest;
 import rs.ac.bg.fon.springsocialnetwork.dto.PostResponse;
 import rs.ac.bg.fon.springsocialnetwork.dto.ReportedPostDto;
+import rs.ac.bg.fon.springsocialnetwork.exception.MyRuntimeException;
 import rs.ac.bg.fon.springsocialnetwork.model.User;
 import rs.ac.bg.fon.springsocialnetwork.service.AuthService;
 import rs.ac.bg.fon.springsocialnetwork.service.PostService;
 
+import javax.validation.Valid;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author UrosVesic
@@ -38,7 +44,7 @@ public class PostController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest postRequest){
+    public ResponseEntity<PostResponse> createPost(@RequestBody @Valid PostRequest postRequest){
         User currentUser = authService.getCurrentUser();
         PostResponse postResponse = postService.createPost(postRequest,currentUser);
         return new ResponseEntity(postResponse,HttpStatus.CREATED);
@@ -84,6 +90,18 @@ public class PostController {
     public ResponseEntity<Set<ReportedPostDto>> getAllSolvedReportedPosts(){
         Set<ReportedPostDto> reportedPosts= postService.getAllSolvedReportedPosts();
         return new ResponseEntity<>(reportedPosts,HttpStatus.OK);
+    }
+
+    @ExceptionHandler(MyRuntimeException.class)
+    public  ResponseEntity<String> handleMyRuntimeException(MyRuntimeException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public  ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<ObjectError> allErrors = ex.getAllErrors();
+        List<String> collect = allErrors.stream().map(err -> err.getDefaultMessage()).collect(Collectors.toList());
+        return new ResponseEntity<>(collect,HttpStatus.BAD_REQUEST);
     }
 
 }
