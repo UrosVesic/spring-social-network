7335349ffca3e8127a0457d2dfc112c28361fd2a
UrosVesic <uros99uki@gmail.com>
2022-07-18 23:23:22 +0200

    Dodat InboxMessageDto i mapper koji mapira ovaj dto u Message

---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/dto/InboxMessageDto.java
@@ -0,0 +1,17 @@
+package rs.ac.bg.fon.springsocialnetwork.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author UrosVesic
+ */
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class InboxMessageDto implements Dto{
+    String with;
+    String content;
+    String time;
+}
---
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/mapper/InboxMessageMapper.java
@@ -0,0 +1,54 @@
+package rs.ac.bg.fon.springsocialnetwork.mapper;
+
+import lombok.AllArgsConstructor;
+import rs.ac.bg.fon.springsocialnetwork.dto.InboxMessageDto;
+import rs.ac.bg.fon.springsocialnetwork.model.Message;
+import rs.ac.bg.fon.springsocialnetwork.service.AuthService;
+
+import java.time.ZoneId;
+
+/**
+ * @author UrosVesic
+ */
+@AllArgsConstructor
+public class InboxMessageMapper implements GenericMapper<InboxMessageDto, Message> {
+
+    private AuthService authService;
+
+    @Override
+    public Message toEntity(InboxMessageDto dto) {
+        return null;
+    }
+
+    @Override
+    public InboxMessageDto toDto(Message entity) {
+        InboxMessageDto dto = new InboxMessageDto();
+        dto.setWith(setWith(entity));
+        dto.setContent(setContent(entity.getContent()));
+        int minute = entity.getSentAt().atZone(ZoneId.of("ECT",ZoneId.SHORT_IDS)).getMinute();
+        String min;
+        if(minute<10){
+            min = "0"+minute;
+        }else{
+            min = minute+"";
+        }
+        dto.setTime(entity.getSentAt().atZone(ZoneId.of("ECT",ZoneId.SHORT_IDS)).getHour()+":"+min);
+        return dto;
+    }
+
+    private String setContent(String content) {
+        if(content.length()>15){
+            String substring = content.substring(0, 15);
+            String finalStr = substring.concat("...");
+            return finalStr;
+        }
+        return content;
+    }
+
+    private String setWith(Message entity) {
+        if(entity.getFrom().getUsername()==authService.getCurrentUser().getUsername()){
+            return entity.getTo().getUsername();
+        }
+        return entity.getFrom().getUsername();
+    }
+}
