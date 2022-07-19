65d2238dd15edc96c7359346b74fe06bb3a5a6aa
UrosVesic <uros99uki@gmail.com>
2022-07-20 01:52:05 +0200

    Slanje obavestenja prilikom reackije

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/service/ReactionService.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/service/ReactionService.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rs.ac.bg.fon.springsocialnetwork.dto.ReactionDto;
+import rs.ac.bg.fon.springsocialnetwork.mapper.NotificationBuilder;
+import rs.ac.bg.fon.springsocialnetwork.mapper.NotificationMapper;
 import rs.ac.bg.fon.springsocialnetwork.mapper.ReactionMapper;
+import rs.ac.bg.fon.springsocialnetwork.model.Notification;
 import rs.ac.bg.fon.springsocialnetwork.model.Reaction;
+import rs.ac.bg.fon.springsocialnetwork.repository.NotificationRepository;
 import rs.ac.bg.fon.springsocialnetwork.repository.ReactionRepository;
 
 import java.util.Optional;
@@ -20,11 +24,17 @@ public class ReactionService {
     private ReactionRepository reactionRepository;
     private ReactionMapper reactionMapper;
     private AuthService authService;
+    private NotificationService notificationService;
+    private NotificationBuilder notificationBuilder;
+
     @Transactional
     public void react(ReactionDto reactionDto){
         Optional<Reaction> reactOpt=reactionRepository.findByPost_idAndUser(reactionDto.getPostId(),authService.getCurrentUser());
+        Reaction reactionEntity = reactionMapper.toEntity(reactionDto);
         if(!reactOpt.isPresent()){
-            reactionRepository.save(reactionMapper.toEntity(reactionDto));
+            reactionRepository.save(reactionEntity);
+            Notification not = notificationBuilder.createNotificationForReaction(reactionEntity);
+            notificationService.save(not);
             return;
         }
         Reaction reaction = reactOpt.get();
@@ -32,7 +42,7 @@ public class ReactionService {
             reactionRepository.delete(reaction);
         }else{
             reactionRepository.delete(reaction);
-            reactionRepository.save(reactionMapper.toEntity(reactionDto));
+            reactionRepository.save(reactionEntity);
         }
 
     }
