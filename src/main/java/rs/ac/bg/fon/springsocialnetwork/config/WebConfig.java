545f85c79d9d15ce5b80aca4ccd45f284e72805d
Uros Vesic <uros.vesic@levi9.com>
2022-11-23 13:49:27 +0100

    Changes

--- src/main/java/rs/ac/bg/fon/springsocialnetwork/config/WebConfig.java
+++ src/main/java/rs/ac/bg/fon/springsocialnetwork/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(appConfig.getFrontUrl())
+                .allowedOriginPatterns("/**")
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
